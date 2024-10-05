564e6defd69f45207cc3e838a940be3fed7e9942
hasbenslain <esiosezequiel@gmail.com>
2024-10-05 11:08:08 +0800

    kaya pakaha ni sir

---
+++ mycopytask.py
@@ -0,0 +1,23 @@
+number = int(input("enter a number: "))
+
+
+positive_numbers = []
+negative_numbers = []
+
+for data in range(number):
+
+    number= int(input("my number"))
+    
+
+for data in number:
+    if data > 0 :
+        positive_numbers.append(number)
+    else:
+        
+         negative_numbers.append(number)
+       
+    print("positive numbers:",positive_numbers)
+              
+    print("negative numbers:",negative_numbers)
+
+ 
